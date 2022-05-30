37cbfb76a866d47fced310463744ba1c07433650
zubairahmedanas <amianasahmed@gmail.com>
2022-05-30 17:39:39 +0600

    category 2(a) meter to feet and feet to meter converted

---
+++ category_2_a.py
@@ -0,0 +1,9 @@
+def meters_to_feet_and_vicevers(numb):
+    meter_to_feet = numb * 3.281
+    print("result of meter to feet is  : ", meter_to_feet)
+    feet_to_meter = numb + 0.3048
+    print("result of feet to meter is :", feet_to_meter)
+
+
+key = int(input('please enter your input : '))
+meters_to_feet_and_vicevers(key)
