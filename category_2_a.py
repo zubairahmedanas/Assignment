4fb0aa4f36884d055c7a2dad65abc288dbd43b9a
zubairahmedanas <amianasahmed@gmail.com>
2022-05-30 17:50:36 +0600

    category 2(a) centimeter to inch ind vice versa converted

--- category_2_a.py
+++ category_2_a.py
@@ -1,9 +1,22 @@
 def meters_to_feet_and_vicevers(numb):
     meter_to_feet = numb * 3.281
-    print("result of meter to feet is  : ", meter_to_feet)
-    feet_to_meter = numb + 0.3048
-    print("result of feet to meter is :", feet_to_meter)
+    print("result of meter is  : ", meter_to_feet)
+    feet_to_meter = numb * 0.3048
+    print("result of feet is :", feet_to_meter)
 
 
 key = int(input('please enter your input : '))
 meters_to_feet_and_vicevers(key)
+
+
+def centimeter_to_inch_and_vicevers(numb):
+    centi_to_inch = numb * 0.393701
+
+    print("result of centimeter is :", centi_to_inch)
+
+    inch_to_centi = numb * 2.54
+    print("result of inch is :", inch_to_centi)
+
+
+key = int(input('please enter your input : '))
+centimeter_to_inch_and_vicevers(key)
